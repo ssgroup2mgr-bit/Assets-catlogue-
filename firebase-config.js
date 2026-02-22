ea35b753683ced743b38f2b6b11e5d76141b0cac
ssgroup2mgr-bit <ss.group2mgr@gmail.com>
2026-02-22 19:10:08 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-// Apni Firebase configuration yahan daalein
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAcBb5Zdtps4dEkL43jrngMBAZ5xI6KhiQ",
-  authDomain: "sstradersbill-108c1.firebaseapp.com",
-  databaseURL: "https://sstradersbill-108c1-default-rtdb.firebaseio.com",
-  projectId: "sstradersbill-108c1",
-  storageBucket: "sstradersbill-108c1.firebasestorage.app",
-  messagingSenderId: "1071080469005",
-  appId: "1:1071080469005:web:991c0e9ccd898a6b395b44",
-  measurementId: "G-DE6PVF37SF"
+  apiKey: "AIzaSyBYTRsQS7DAU1iCRxtVrcDb_vPE6pzrpy4",
+  authDomain: "catlogue-fa34b.firebaseapp.com",
+  projectId: "catlogue-fa34b",
+  storageBucket: "catlogue-fa34b.firebasestorage.app",
+  messagingSenderId: "1004660298794",
+  appId: "1:1004660298794:web:66468bb6b341d1ecbb5f53"
 };
 
 // Initialize Firebase
